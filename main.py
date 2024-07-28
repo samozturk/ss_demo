ecf22e2e1d0accc6132d1a766187f9a4f22b90d7
Sam <ssamozturk@gmail.com>
2024-07-28 11:39:03 +0200

    dev: initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+import random
+
+european_cities = [
+    "Porto",
+    "Krakow",
+    "Edinburgh",
+    "Dubrovnik",
+    "Reykjavik",
+    "Bruges",
+    "Salzburg",
+    "Tallinn",
+    "Seville",
+    "Bologna",
+    "Ljubljana",
+    "Zurich",
+    "Copenhagen",
+    "Bratislava",
+    "Gothenburg",
+    "Marseille",
+    "Bucharest",
+    "Cologne",
+    "Riga",
+    "Thessaloniki"
+]
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/cities")
+async def test(n:int = 5):
+    random_cities = random.sample(european_cities, n)
+    return {"random_cities": random_cities}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
